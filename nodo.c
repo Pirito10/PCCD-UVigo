73a0c90522459625a6ef64a416c7dec2d1ad94d7
Manuel MV <manuelmartinezvaamonde@gmail.com>
2024-05-03 20:46:42 +0200

    Añadido gitignore

--- nodo.c
+++ nodo.c
@@ -1,3 +1,6 @@
+// Sin esta línea se rompe mi entorno, se puede suprimir en la entrega final pero la necesito para debuggear -Manu
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
