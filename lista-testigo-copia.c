cdbe9b204de2e9e5de81800d4f9001a356d4f0b1
Iván <pillado.rodriguez.ivan@gmail.com>
2024-05-04 01:45:27 +0200

    Añadido función quitar_lista para poder quitar id del nodo que estea en la lista

--- lista-testigo-copia.c
+++ lista-testigo-copia.c
@@ -11,8 +11,8 @@
 
 
 int crear_lista();
-int añadir_lista();
-int quitar_lista();
+int añadir_lista(int id);
+int quitar_lista(int id);
 int lista_vacia();
 
 struct NodoLista *nodo_cabeza = NULL;   //Puntero que apunta al primer nodo de la lista
@@ -36,6 +36,33 @@ int añadir_lista(int id){
         nodoNuevo->id = id;
         nodo_cabeza = nodoNuevo;
         }
+    return 0;
 }
 
-        
+int quitar_lista(int id){
+    if(nodo_cabeza == NULL){
+        return -1;
+        //La lista está vacía
+    }
+
+    struct NodoLista *nodo_actual = nodo_cabeza;
+    struct NodoLista *nodo_anterior = NULL;
+
+    while(nodo_actual != NULL && nodo_actual->id != id){
+        nodo_anterior = nodo_actual;
+        nodo_actual = nodo_actual->sig;
+    }
+
+    if(nodo_actual == NULL){
+        //El id no existe en la lista
+        return -2;
+    }
+    if(nodo_anterior == NULL){
+        nodo_cabeza = nodo_actual->sig;
+    }
+    else{
+        nodo_anterior->sig = nodo_actual->sig;
+    }
+    free(nodo_actual);
+    return 0;
+}
