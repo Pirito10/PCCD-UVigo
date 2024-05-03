075d677083b5319d4924fe0598d2367dfd5e85ff
Manuel MV <manuelmartinezvaamonde@gmail.com>
2024-05-03 22:47:20 +0000

    launch params

---
+++ cliente.c
@@ -0,0 +1,6 @@
+// Sin esta línea se rompe mi entorno, se puede suprimir en la entrega final pero la necesito para debuggear -Manu
+#define _GNU_SOURCE
+
+int main(int argc, char *argv[]) {
+    
+}
