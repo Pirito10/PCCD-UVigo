9fdf1c9df3fc645d9815ca9dc052989f2c623ad5
Pirito10 <aaronriveiro@gmail.com>
2024-05-04 22:50:19 +0000

    terminado cliente.c

--- cliente.c
+++ cliente.c
@@ -1,6 +1,69 @@
 // Sin esta línea se rompe mi entorno, se puede suprimir en la entrega final pero la necesito para debuggear -Manu
 #define _GNU_SOURCE
 
-int main(int argc, char *argv[]) {
-    
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+
+// Estructura de los mensajes
+struct msg_nodo
+{
+    long mtype; // Tipo de mensaje, 11 -> pago, 12 -> anulacion, 13 -> reserva, 14 -> administracion, 15 -> consulta
+};
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Debes introducir el número de nodos del sistema: ./cliente <N>\n");
+        return EXIT_FAILURE;
+    }
+
+    // Tipo de consulta e ID del nodo receptor
+    int tipo_solicitud, ID;
+    int N = atoi(argv[1]);
+
+    if (N < 0)
+    {
+        printf("El número de nodos debe ser mayor que cero\n");
+        return EXIT_FAILURE;
+    }
+
+    // Bucle infinito
+    while (1)
+    {
+        // Tomamos el ID
+        printf("Introduce el ID del nodo al que quieres enviar la solicitud (0-%d), o -1 para salir: ", N);
+        scanf("%d", &ID);
+
+        if (ID == -1)
+        {
+            return EXIT_SUCCESS;
+        }
+
+        if (ID > N || ID < -1)
+        {
+            printf("Has seleccionado un nodo que no existe\n\n");
+            continue;
+        }
+
+        // Tomamos el tipo de solicitud
+        printf("Selecciona el tipo de solicitud:\n- Pago: 1\n- Anulación: 2\n- Reserva: 3\n- Administración: 4\n- Consulta: 5\n");
+        scanf("%d", &tipo_solicitud);
+
+        // Obtenemos la cola de mensajes del nodo
+        int msgid = msgget(ID, 0666 | IPC_CREAT);
+
+        // Creamos el mensaje
+        struct msg_nodo msg_nodo;
+        msg_nodo.mtype = tipo_solicitud + 10; // Ver la defición del struct
+
+        // Enviamos el mensaje
+        msgsnd(msgid, &msg_nodo, sizeof(msg_nodo), 0);
+
+        printf("Solicitud enviada\n\n");
+    }
 }
